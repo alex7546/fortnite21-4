e3d69e0682c902b7432a311b284d812520f242ba
alex7546 <31044532+alex7546@users.noreply.github.com>
2020-12-31 08:42:36 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+var ogads = document.querySelector('.ogads');
+
+ogads.forEach((elt)=>{
+  elt.addEventListener('click',()=>{
+    window.location.href="https://www.locked3.com/cl.php?id=72337f88020f91c69081fb7ef4ad295a";
+  });
+});
+
